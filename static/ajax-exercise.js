a8a67a32b46666da3464fbf44983065f61a0affa
Enid-Sky <enid.sky@protonmail.com>
2020-10-22 13:40:36 -0700

    complete showFortune and showWeather

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -1,17 +1,15 @@
 "use strict";
 
-
 // PART 1: SHOW A FORTUNE
 
 function showFortune(evt) {
 
-    // TODO: get the fortune and show it in the #fortune-text div
+    $.get('/fortune', (res) => {
+        $('#fortune-text').html(res);
+
+});
 }
-
 $('#get-fortune-button').on('click', showFortune);
-
-
-
 
 
 // PART 2: SHOW WEATHER
@@ -22,8 +20,15 @@ function showWeather(evt) {
     let url = "/weather.json";
     let formData = {"zipcode": $("#zipcode-field").val()};
 
+//get from "/weather.json", form data variable, (res)
+//weather-info.html (res.forecast)
 
-    // TODO: request weather with that URL and show the forecast in #weather-info
+    //  if formData "empty":
+        // flash("Please enter a zip code")
+    // else:
+        $.get('/weather.json', formData, (res) => {
+            $('#weather-info').html (res.forecast);
+        });
 }
 
 $("#weather-form").on('submit', showWeather);
@@ -38,8 +43,20 @@ function orderMelons(evt) {
 
     // TODO: show the result message after your form
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+
+    // create form inputs, make a dictionary 
+        //melon_qty: #qty_field.val(),  type: #melon_type_field.val(),
+        // order_status: '#order-status'
+
+    //  if order-status.val() ==== ERROR:
+            // .addClass('color', 'red' or .order-error ?)  
+
+    // $.post('/order-melons.json, formInput, (res) => {
+        // $(# ????).html(res, ????)
+    }
 }
 
 $("#order-form").on('submit', orderMelons);
+
 
 
